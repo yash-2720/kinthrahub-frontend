66e724e07d56bf6aebdcf0ee0f5225da00d96257
yash-2720 <mnyashwanthkumar27@gmail.com>
2026-07-21 12:02:26 +0530

    Angular configuration and login setup

---
+++ src/app/core/services/authentication.service.ts
@@ -0,0 +1,11 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+
+  constructor(private http: HttpClient) { }
+}
