6574689984de3faa73b1d41503e4d282488ec0d9
yash-2720 <mnyashwanthkumar27@gmail.com>
2026-08-12 23:38:15 +0530

    Donation start date and end date radio buttons added,Validators added for Donation start date

--- src/app/features/donation-request/configure-donations/configure-donations/configure-donations.ts
+++ src/app/features/donation-request/configure-donations/configure-donations/configure-donations.ts
@@ -1,14 +1,20 @@
 import { Component, Input,  OnInit } from '@angular/core';
 import  { DonationWorkflowItem } from '../../models/donation-workflow-item';
-import  { FormBuilder, FormGroup, type FormArray } from '@angular/forms';
+import  { FormBuilder, FormGroup, ReactiveFormsModule, Validators, type FormArray } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { DonationType } from '../../enums/donation-type.enum';
+import {MatRadioModule} from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-configure-donations',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, MatRadioModule,MatDatepickerModule, MatNativeDateModule  ],
   templateUrl: './configure-donations.html',
   styleUrl: './configure-donations.css',
 })
 export class ConfigureDonations implements OnInit {
+DonationType = DonationType ;
   constructor(private fb : FormBuilder){}
 
   @Input()
@@ -26,6 +32,7 @@ export class ConfigureDonations implements OnInit {
       });
       this.initializeDonationForms();
       console.log("Donations Length : " ,this.donations.length) ;
+      console.log("Donations :" ,this.donations);
   }
 
   get donations(): FormArray{
@@ -39,15 +46,39 @@ export class ConfigureDonations implements OnInit {
         hospitalName : [item.hospitalName],
         donationPlanId : [item.donationPlanId],
         donationName : [item.donationName],
-        donationAmount : [''],
-        donationType : [''],
+        donationAmount : ['',[Validators.required, Validators.min(500), Validators.pattern(/^[0-9]+$/)]],
+        donationType : [DonationType.RECURRING],
         donationStartDate : [''],
         donationEndDate : [''],
         description : ['']
       });
+      donationForm.get('donationType')?.valueChanges.subscribe((value) => {
+        if(value === DonationType.RECURRING) {
+          donationForm.get('donationStartDate')?.setValidators([Validators.required]);
+          donationForm.get('donationStartDate')?.updateValueAndValidity();
+          // donationForm.get('donationEndDate')?.setValidators([Validators.required]);
+        }else{
+
+          donationForm.get('donationStartDate')?.clearValidators();
+          donationForm.get('donationStartDate')?.setValue('');
+          donationForm.get('donationEndDate')?.setValue(null);
+          donationForm.get('donationStartDate')?.updateValueAndValidity();
+
+        }});
+
       this.donations.push(donationForm);
     } ))
   }
   
+  isDonationTypeRecurring(index: number): boolean {
+    const donationForm = this.donations.at(index);
+    if(donationForm.get('donationType')?.value === DonationType.RECURRING){
+      console.log("Inside if block",this.donations.at(0).get('donationType')?.value)
+      return true;
+    }
+    console.log(this.donations.at(0).get('donationType')?.value)
+    return false;
+  }
 
+ 
 }
